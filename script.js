9e19203f68c8079e7e5d3ef0bfea49375318c3fd
Maxik <mpoplavskiy38380@gmail.com>
2023-09-20 17:59:25 +0300

    Change cookie delete

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function onLoad() {
 
         if(cookieScript3Value !== undefined) {
             alert(`Script3 cookies = ${cookieScript3Value}, cookies will be deleted after "OK" button click`);
-            document.cookie = ``;
+            document.cookie = `username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
             alert(`Cookies were deleted`);
         }
     }
