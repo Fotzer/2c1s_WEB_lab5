42b23a3c27b24fe36698eca61b9cbe0964d6155e
Maxik <mpoplavskiy38380@gmail.com>
2023-09-20 15:17:38 +0300

    Add number value handling

--- script.js
+++ script.js
@@ -8,13 +8,25 @@ function swap25() {
 
 document.getElementById("script1-button").addEventListener("click", swap25);
 
-function pentagonArea() {
-    const side = Number(document.getElementById("script2-input").value);
-    const area = Math.sqrt(5*(5+2*Math.sqrt(5)))*(side**2)/4;
 
+function pentagonArea(event) {
+    const side = Number(document.getElementById("script2-input").value);
     const script2Result = document.getElementById("script2-result");
+
+    if(side <= 0) {
+        script2Result.innerHTML = `Script 2 result: Invalid number`;
+    } else {
+        const area = Math.sqrt(5*(5+2*Math.sqrt(5)))*(side**2)/4;
+        script2Result.innerHTML = `Script 2 result: ${area}`;
+    }
     
-    script2Result.innerHTML = `Script 2 result: ${area}`;
 }
 
 document.getElementById("script2-button").addEventListener("click", pentagonArea);
+
+
+function script3() {
+
+}
+
+document.getElementById("script2-button").addEventListener("click", script3);
